858ec5fc7b43a5b32a1582af1712c8174cfafbe8
Matthew Barnhart <mcb4rnh4rt@gmail.com>
2020-02-29 22:10:11 -0600

    Passed problem 5!

--- starter-code/app.js
+++ starter-code/app.js
@@ -109,11 +109,11 @@ function multiplyArray(multArr) { //eslint-disable-line
   console.log(x);
   var product = multiply(x,multArr[2])[0];
   console.log(product);
-  return[product, multArr[0] + ',' + multArr[1] + ',' + multArr[2] + ' have a product of ' + product + '.'];
+  return[product, 'The numbers ' + multArr[0] + ',' + multArr[1] + ',' + multArr[2] + ' have a product of ' + product + '.'];
 }
 
 // Here is the test for multiplyArray(); uncomment it to run it
-// testMultiplyArray(testArray);
+testMultiplyArray(testArray);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. 
 
