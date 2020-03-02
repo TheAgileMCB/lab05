dd76732cd015f9358aa1de8f2454f48a5f7760d0
Matthew Barnhart <mcb4rnh4rt@gmail.com>
2020-03-01 20:43:10 -0600

    working on problem 6

--- starter-code/app.js
+++ starter-code/app.js
@@ -135,7 +135,9 @@ Test this function by hand in the console to get it working, and when you think 
 var testDynamicArray = [1,2,3,4,5]; //eslint-disable-line
 
 function multiplyAnyArray(dynamicArray) { //eslint-disable-line
-
+  for(var i =0; i <dynamicArray.length;i++){
+    dynamicArray = [1,2,3,4,5];
+    
 }
 
 // Here is the test for multiplyArray(); uncomment it to run it
